dee1100ce6afd484d679a28c6855e168153384aa
Rafael Laverde <leafar91@gmail.com>
2016-08-24 12:38:44 -0500

    Add slick-carousel (js, sass)

--- webpack/js/main.js
+++ webpack/js/main.js
@@ -1,5 +1,6 @@
 foundation = require('foundation-sites/dist/foundation.min.js');
 require("font-awesome-loader");
+require('slick-carousel')
 
 $(document).ready(function ($) {
   $(document).foundation();
