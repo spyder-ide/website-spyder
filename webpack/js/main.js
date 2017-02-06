00506e9410818e2dbc02aa8baf94de638dd0007b
Rafael Laverde <leafar91@gmail.com>
2017-02-06 14:06:11 -0500

    Add user section - Add users to poster model - Some modifications to slideshow - Create users slideshow

--- webpack/js/main.js
+++ webpack/js/main.js
@@ -10,5 +10,32 @@ $(document).ready(function ($) {
     autoplaySpeed: 2000,
 });
 
+  $('.carousel-users').slick({
+    autoplay: true,
+    infinite: true,
+    arrows: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplaySpeed: 5000,
+
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+
+  });
+
   $(document).foundation();
 });
